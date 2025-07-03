e4ee3dbe0a627daf3b009927de353cc4bf01f62f
Nech-C <xiaokunchen@outlook.com>
2025-07-03 16:28:02 -0400

    feat: improve sentiment weighting using log1p + tem softmax

--- app/processing/aggregate.py
+++ app/processing/aggregate.py
@@ -5,6 +5,13 @@ import heapq
 import numpy as np
 
 
+def normalized_softmax(x: np.ndarray, temperature: int) -> np.ndarray:
+    x = np.log1p(x)
+    x /= temperature
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+
 def compute_sentiment_average(posts: list[dict]) -> dict:
     """
     Aggregates sentiment scores across all posts.
@@ -20,28 +27,24 @@ def compute_sentiment_average(posts: list[dict]) -> dict:
         return {}
 
     # softmax
-    temperature = 1000  # Try tuning between 100–5000
+    temperature = 1.5  # Try tuning between 100–5000
 
-    post_scores = np.array([max(post["score"], 0) for post in valid_posts])
-    max_post_score = max(post_scores)
-
-    normalized_post_scores = post_scores / temperature
-    normalized_post_scores = post_scores - max_post_score
-
-    weights = np.exp(normalized_post_scores)
-    weights /= np.sum(weights)
-    print(f"weights: {weights}")
+    filtered = [(i, max(p["score"], 0)) for i, p in enumerate(valid_posts) if p["score"] > 0]
+    indices, scores = zip(*filtered) if filtered else ([], [])
+    scores = np.array(scores)
+    weights = normalized_softmax(scores, temperature)
+    
     weighted_totals = defaultdict(float)
     top_contributors = defaultdict(list)
 
-    for idx, post in enumerate(valid_posts):
+    for i, w in zip(indices, weights):
+        post = valid_posts[i]
         sentiment = post.get("sentiment")
-        weight = weights[idx]
         for k, v in sentiment.items():
-            contribution = v * weight
+            contribution = v * w
             weighted_totals[k] += contribution
 
-            heapq.heappush(top_contributors[k], (contribution, idx, post))
+            heapq.heappush(top_contributors[k], (contribution, i, post))
 
             if len(top_contributors[k]) > 3:
                 heapq.heappop(top_contributors[k])
@@ -49,6 +52,7 @@ def compute_sentiment_average(posts: list[dict]) -> dict:
     total = sum(weighted_totals.values())
     if total == 0:
         return {k: 0 for k in weighted_totals}
+
     average = {k: v / total for k, v in weighted_totals.items()}
 
     return {
